a695234467a8318fa965568481325b0172981d5c
Anna <fesun.anna@lll.kpi.ua>
2024-05-27 23:21:57 +0300

    changed class button

--- interface.py
+++ interface.py
@@ -1,32 +1,31 @@
 import pygame
+import time
 from pygame.locals import *
+from game import Game, save_board_to_file
+from solver import Solver
+import threading
 
 class Button:
-    def __init__(self, text, pos, size, font, fg_color, bg_color, hover_color, callback):
-        self.text = text
-        self.pos = pos
-        self.size = size
-        self.rect = pygame.Rect(pos, size)
-        self.font = font
-        self.fg_color = fg_color
-        self.bg_color = bg_color
-        self.hover_color = hover_color
-        self.callback = callback
-        self.hovered = False
+    def __init__(self, text, rect, font, fg_color, bg_color, callback):
+        self.text = text  # Текст кнопки
+        self.rect = rect  # Прямокутник, який представляє кнопку
+        self.font = font  # Шрифт кнопки
+        self.fg_color = fg_color  # Колір тексту кнопки
+        self.bg_color = bg_color  # Колір фону кнопки
+        self.callback = callback  # Функція зворотного виклику, яка виконується при натисканні кнопки
+        self.running = True  # Прапорець, що вказує, чи триває робота кнопки
 
     def draw(self, surface):
-        current_color = self.hover_color if self.hovered else self.bg_color
-        pygame.draw.rect(surface, current_color, self.rect, border_radius=10)
-        text_surface = self.font.render(self.text, True, self.fg_color)
-        text_rect = text_surface.get_rect(center=self.rect.center)
-        surface.blit(text_surface, text_rect)
+        pygame.draw.rect(surface, self.bg_color, self.rect)  # Відображення фону кнопки
+        text_surface = self.font.render(self.text, True, self.fg_color)  # Створення поверхні з текстом кнопки
+        text_rect = text_surface.get_rect(center=self.rect.center)  # Вирівнювання тексту кнопки по центру прямокутника кнопки
+        surface.blit(text_surface, text_rect)  # Відображення тексту кнопки на екрані
 
     def handle_event(self, event):
-        if event.type == pygame.MOUSEMOTION:
-            self.hovered = self.rect.collidepoint(event.pos)
-        elif event.type == pygame.MOUSEBUTTONDOWN:
-            if self.rect.collidepoint(event.pos):
-                self.callback()
+        if event.type == pygame.MOUSEBUTTONDOWN:  # Перевірка на натискання кнопки мишею
+            if self.rect.collidepoint(event.pos):  # Перевірка, чи позиція натискання знаходиться в межах прямокутника кнопки
+                self.callback()  # Виклик функції
                 return True
         return False
+
 
